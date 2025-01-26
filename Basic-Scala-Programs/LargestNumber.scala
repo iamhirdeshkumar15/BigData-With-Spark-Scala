3ce80e9ea2405a631a1d41e3507000a5bbc1525a
Hirdesh Kumar Yadav <125953445+iamhirdeshkumar15@users.noreply.github.com>
2025-01-26 21:07:18 +0530

    Add LargestNumber.scala: Program to find the largest number among given values.

---
+++ Basic-Scala-Programs/LargestNumber.scala
@@ -0,0 +1,14 @@
+
+
+object LargestNumber {
+  def main(args: Array[String]){
+    val num1 = 23
+    val num2 = 20
+    if ( num1 > num2) {
+      println(s"$num1 is greater than $num2")
+    }
+    else {
+      println(s"$num2 is greater than $num1")
+    }
+  }
+}
