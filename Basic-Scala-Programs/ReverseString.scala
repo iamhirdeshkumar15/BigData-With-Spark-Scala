8b08e8ad3db8877545d5daed43ef8767a353a638
Hirdesh Kumar Yadav <125953445+iamhirdeshkumar15@users.noreply.github.com>
2025-01-27 15:51:51 +0530

    Add ReverseString.scala: Program to reverse a given string.

---
+++ Basic-Scala-Programs/ReverseString.scala
@@ -0,0 +1,9 @@
+
+
+object ReverseString {
+  def main(args: Array[String]) {
+    val str = "Scala"
+    val reversed = str.reverse
+    println(s"Reversed string of '$str' is '$reversed'")
+  }
+}
