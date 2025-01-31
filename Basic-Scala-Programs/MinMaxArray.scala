a6fb4eb8236015798e4dfc3c81c191de5290e1a5
Hirdesh Kumar Yadav <125953445+iamhirdeshkumar15@users.noreply.github.com>
2025-01-31 15:01:35 +0530

    Add MinMaxArray.scala: Program to find the minimum and maximum elements in an array.

---
+++ Basic-Scala-Programs/MinMaxArray.scala
@@ -0,0 +1,11 @@
+
+
+object MinMaxArray {
+  def main(args: Array[String]) {
+    val arr = Array(23, 56 , 1, 8, 14)
+    val min = arr.min
+    val max = arr.max
+    println(s"Minimum value in array: $min")
+    println(s"Maximum value in array: $max")
+  }
+}
