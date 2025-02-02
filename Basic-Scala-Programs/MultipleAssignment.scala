ea02f7af8ce5c53cdd007bce047c98bb5aae79c0
Hirdesh Kumar Yadav <125953445+iamhirdeshkumar15@users.noreply.github.com>
2025-02-02 13:23:15 +0530

    Added MultipleAssignment.scala: Demonstrates multiple variable assignment in Scala.

---
+++ Basic-Scala-Programs/MultipleAssignment.scala
@@ -0,0 +1,11 @@
+
+
+object MultipleAssignment {
+  def main(args: Array[String]) {
+    val (name, age) = ("Kumar", 25)
+    println(s"Name: $name, Age: $age")
+    
+    val (x, y, z) = (1, 2.5, "Kumar")
+    println(s"x: $x, y: $y, z: $z")
+  }
+}
