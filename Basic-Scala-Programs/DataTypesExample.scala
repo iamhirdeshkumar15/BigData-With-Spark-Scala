c6a93e4d5e46b7acc991caa29cdbbd11791a1123
Hirdesh Kumar Yadav <125953445+iamhirdeshkumar15@users.noreply.github.com>
2025-02-02 13:18:21 +0530

    Added DataTypesExample.scala: Covers different data types in Scala, including Int, Double, String, and Boolean.

---
+++ Basic-Scala-Programs/DataTypesExample.scala
@@ -0,0 +1,13 @@
+
+
+object DataTypesExample {
+  def main(args: Array[String]) {
+    val intVal: Int = 15
+    val doubleVal: Double = 3.14
+    val charVal: Char = 'A'
+    val stringVal: String = "Kumar"
+    val boolVal: Boolean = true
+    
+    println(s"Int: $intVal, Double: $doubleVal, Char: $charVal, String: $stringVal, Boolean: $boolVal")
+  }
+}
