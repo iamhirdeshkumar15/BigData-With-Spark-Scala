7d48395c83933f6554cd253c04c7f6231bdb8854
Hirdesh Kumar Yadav <125953445+iamhirdeshkumar15@users.noreply.github.com>
2025-02-02 13:20:45 +0530

    Added TypeInference.scala: Highlights Scala's type inference mechanism.

--- Basic-Scala-Programs/TypeInference.scala
+++ Basic-Scala-Programs/TypeInference.scala
@@ -2,11 +2,12 @@
 
 object TypeInference {
   def main(args: Array[String]) {
-    val message = "Scala is powerful" //Inferred as String
-    val year = 2024 // Inferred as Int
-    val pi = 3.14 // Inferred as Double
+    val message = "Scala is powerful" // Scala infers type as String
+    val year = 2024 // Scala infers type as Int
+    val pi = 3.14 // Scala infers type as Double
+    val inferredString = "Type Inference" // Scala infers type as String
     
-    println(s"Message: $message, Year: $year, Pi: $pi")
+    println(s"Message: $message, Year: $year, Pi: $pi, String: $inferredString")
     
   }
 }
