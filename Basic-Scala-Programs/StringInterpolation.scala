a57dcbc4d94638a53f792e43cee5c08a9d129bb8
Hirdesh Kumar Yadav <125953445+iamhirdeshkumar15@users.noreply.github.com>
2025-02-02 13:23:19 +0530

    Added StringInterpolation.scala: Explains string interpolation using s, f, and raw interpolators.

--- Basic-Scala-Programs/StringInterpolation.scala
+++ Basic-Scala-Programs/StringInterpolation.scala
@@ -4,8 +4,15 @@ object StringInterpolation {
   def main(args: Array[String]) {
     val name = "Kumar"
     val age = 25
+    val height = 5.9
     
+    // S-Interpolation
     println(s"My name is $name and I am $age years old.")
+    // F-Interpolation (for formatted strings)
     println(f"Pi value is approximately ${math.Pi}%.2f")
+    
+    // Raw-Interpolation
+    println(raw"New Line Example: \n This will not be interpreted")
+    
   }
 }
