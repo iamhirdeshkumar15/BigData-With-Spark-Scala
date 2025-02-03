13a9f897bd6363340bb90692ff5a32246a59db0c
Hirdesh Kumar Yadav <125953445+iamhirdeshkumar15@users.noreply.github.com>
2025-02-03 15:20:41 +0530

    Added while loop demonstration in Scala.

---
+++ Basic-Scala-Programs/WhileLoop.scala
@@ -0,0 +1,11 @@
+// Added while loop demonstration in Scala.
+
+object WhileLoop {
+  def main(args: Array[String]) {
+    var count = 1
+    while (count <= 5) {
+      println(s"While Loop Count: $count")
+      count += 1
+    }
+  }
+}
